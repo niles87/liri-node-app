d4af9904aaeea2aeca00754daf56123d8e6fdf45
niles87 <nbing19@gmail.com>
2020-02-05 22:34:21 -0800

    changed initial if statements to switch case, added functions to call each saved command, added an append to file function to log all the commands and arguments used with a date and time for tracking

--- liri.js
+++ liri.js
@@ -6,7 +6,9 @@ var axios = require("axios");
 
 var moment = require("moment");
 
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+
+var fs = require("fs");
 
 var liriArgs = process.argv.slice(3).join(" ");
 console.log(liriArgs);
@@ -18,14 +20,36 @@ var movie = "movie-this";
 
 var whatItSays = "do-what-it-says";
 
-if (process.argv[2] === concert) {
+switch (process.argv[2]) {
+  case concert:
+    logCommands();
+    concertSearch();
+    break;
+
+  case song:
+    logCommands();
+    songSearch();
+    break;
+
+  case movie:
+    logCommands();
+    movieSearch();
+    break;
+
+  case whatItSays:
+    logCommands();
+    wildCard();
+    break;
+}
+
+function concertSearch() {
   var queryURL =
     "https://rest.bandsintown.com/artists/" + liriArgs + "/events?app_id=codingbootcamp";
 
   console.log(queryURL);
   axios
     .get(queryURL)
-    .then(function(response) {
+    .then(response => {
       response.data.forEach(element => {
         console.log("*****venue data*****");
         console.log(element.venue.name);
@@ -34,31 +58,160 @@ if (process.argv[2] === concert) {
         console.log("*****venue end*****");
       });
     })
-    .catch(function(err) {
+    .catch(err => {
       console.log(err);
     });
 }
 
-if (process.argv[2] === movie) {
-  var queryURL =
-    "http://www.omdbapi.com/?t=" + liriArgs.split(" ").join("+") + "&y=&plot=short&apikey=trilogy";
-  console.log(queryURL);
+function movieSearch() {
+  if (liriArgs) {
+    var queryURL =
+      "http://www.omdbapi.com/?t=" +
+      liriArgs.split(" ").join("+") +
+      "&y=&plot=short&apikey=trilogy";
+    console.log(queryURL);
 
-  axios
-    .get(queryURL)
-    .then(function(response) {
-      console.log("+++++++movie data+++++++");
-      console.log(response.data.Title);
-      console.log(response.data.Year);
-      console.log(response.data.imdbRating);
-      console.log(response.data.Ratings[1].Source, response.data.Ratings[1].Value);
-      console.log(response.data.Country);
-      console.log(response.data.Language);
-      console.log(response.data.Plot);
-      console.log(response.data.Actors);
-      console.log("+++++++movie end+++++++");
-    })
-    .catch(function(err) {
-      console.log(err.config);
-    });
+    axios
+      .get(queryURL)
+      .then(response => {
+        console.log("+++++++movie data+++++++");
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.imdbRating);
+        console.log(response.data.Ratings[1].Source, response.data.Ratings[1].Value);
+        console.log(response.data.Country);
+        console.log(response.data.Language);
+        console.log(response.data.Plot);
+        console.log(response.data.Actors);
+        console.log("+++++++movie end+++++++");
+      })
+      .catch(err => {
+        console.log(err.config);
+      });
+  } else {
+    axios
+      .get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy")
+      .then(response => {
+        console.log("+++++++movie data+++++++");
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.imdbRating);
+        console.log(response.data.Ratings[1].Source, response.data.Ratings[1].Value);
+        console.log(response.data.Country);
+        console.log(response.data.Language);
+        console.log(response.data.Plot);
+        console.log(response.data.Actors);
+        console.log("+++++++movie end+++++++");
+      })
+      .catch(err => {
+        console.log(err.config);
+      });
+  }
+}
+
+function songSearch() {
+  var spotify = new Spotify(keys.spotify);
+
+  if (liriArgs) {
+    spotify
+      .search({ type: "track", query: liriArgs, limit: 1 })
+      .then(response => {
+        var search = response.tracks.items[0];
+        console.log("=======spotify data=======");
+        console.log(search.album.artists[0].name);
+        console.log(search.name);
+        console.log(search.preview_url);
+        console.log(search.album.name);
+        console.log("=======spotify end=======");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else {
+    spotify
+      .search({ type: "track", query: "The Sign", limit: 1 })
+      .then(response => {
+        var search = response.tracks.items[0];
+        console.log("=======spotify data=======");
+        console.log(search.album.artists[0].name);
+        console.log(search.name);
+        console.log(search.preview_url);
+        console.log(search.album.name);
+        console.log("=======spotify end=======");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
+
+function wildCard() {
+  fs.readFile("random.txt", "utf8", (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    var dataArr = data.split(",");
+
+    switch (dataArr[0]) {
+      case "spotify-this-song":
+        var spotify = new Spotify(keys.spotify);
+        spotify.search({ type: "track", query: dataArr[1], limit: 1 }).then(response => {
+          var search = response.tracks.items[0];
+          console.log("=======spotify data=======");
+          console.log(search.album.artists[0].name);
+          console.log(search.name);
+          console.log(search.preview_url);
+          console.log(search.album.name);
+          console.log("=======spotify end=======");
+        });
+        break;
+      case "movie-this":
+        axios
+          .get("http://www.omdbapi.com/?t=" + dataArr[1] + "&y=&plot=short&apikey=trilogy")
+          .then(response => {
+            console.log("+++++++movie data+++++++");
+            console.log(response.data.Title);
+            console.log(response.data.Year);
+            console.log(response.data.imdbRating);
+            console.log(response.data.Ratings[1].Source, response.data.Ratings[1].Value);
+            console.log(response.data.Country);
+            console.log(response.data.Language);
+            console.log(response.data.Plot);
+            console.log(response.data.Actors);
+            console.log("+++++++movie end+++++++");
+          })
+          .catch(err => {
+            console.log(err.config);
+          });
+        break;
+      case "concert-this":
+        axios
+          .get(
+            "https://rest.bandsintown.com/artists/" + dataArr[1] + "/events?app_id=codingbootcamp"
+          )
+          .then(response => {
+            response.data.forEach(element => {
+              console.log("*****venue data*****");
+              console.log(element.venue.name);
+              console.log(element.venue.city + " , " + element.venue.country);
+              console.log(moment(element.datetime).format("MM/DD/YYYY"));
+              console.log("*****venue end*****");
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+    }
+  });
+}
+
+function logCommands() {
+  var commands =
+    "\n" + process.argv[2] + " , " + liriArgs + " , " + moment().format("MM/DD/YYYY, hh:mm:ss A");
+  fs.appendFile("log.txt", commands, error => {
+    if (error) {
+      return console.log(error);
+    }
+  });
 }
