c51c05859d4433458a4a37f8492354a84be32f6c
niles87 <nbing19@gmail.com>
2020-02-06 18:00:38 -0800

    added apikeys to list

--- keys.js
+++ keys.js
@@ -1,6 +1,8 @@
 console.log("this is loaded");
 
-exports.spotify = {
+exports.apiKeys = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET,
+  omdb: process.env.OMDB,
+  bandsInTown: process.env.BANDSINTOWN,
 };
